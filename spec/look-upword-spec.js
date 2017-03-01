eae401d6db8f49122da9eb59bfd664faf296ebcf
Winston <winston.j.zhao@gmail.com>
2017-03-01 12:23:49 -0800

    unit testing working

--- spec/look-upword-spec.js
+++ spec/look-upword-spec.js
@@ -3,17 +3,15 @@ var lookUpword = require('../look-upword').lookUpword
 describe('basic upword', function () {
     var callbacks = {
         suggestionCallback: function (res) {
-            console.log(res)
         },
         wordsCallback: function (res) {
-            console.log(res)
         }
     }
     beforeEach(function (done) {
         spyOn(callbacks, 'wordsCallback').and.callThrough()
         lookUpword('?key=0b966b02-dd99-4a31-a735-2206edb9a8a5', 'value', callbacks.suggestionCallback, callbacks.wordsCallback, done)
     })
-    
+
     it('returns words', function () {
         // callbacks.wordsCallback('loud')
         expect(callbacks.wordsCallback.calls.any()).toEqual(true)
